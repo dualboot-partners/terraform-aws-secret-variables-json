6431a5496d6d588272d550b8cac3d7d8a32cbb58
Alex Astafyev <av.astafyev@gmail.com>
2021-02-02 16:20:08 +0400

    added ssm parameters encryption

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
+resource "aws_kms_key" "encryption_key" {
+  description             = "This key is used to encrypt SSM '${var.ssm_key_prefix}' parameters"
+  deletion_window_in_days = 10
+}
+
 resource "aws_ssm_parameter" "secret_var" {
   for_each = var.map
 
   name        = "/${var.ssm_key_prefix}/${each.key}"
   type        = "SecureString"
   overwrite   = true
+  key_id      = aws_kms_key.encryption_key.arn
   value       = each.value
 }
